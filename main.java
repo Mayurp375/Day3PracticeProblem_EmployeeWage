ea724583e268d382c722b5af01387e9c63653785
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 17:15:45 +0530

    solved using case

--- main.java
+++ main.java
@@ -8,24 +8,30 @@ public class main {
         int wagePerHour = 20;
         int fullDayHour = 8;
         System.out.println("welcome to employee wage");
+        //Random class
         Random random = new Random();
 
+        //object of the random
         int presenty = random.nextInt(3);
-        System.out.println(presenty);
-        if (presenty == 1) {
-            System.out.println("present");
-             wagePerHour = 20;
-             fullDayHour = 8;
-        } else if(presenty ==2) {
-            System.out.println("absent");
-            wagePerHour = 0;
-            fullDayHour = 0;
-        }else {
-            System.out.println("partTime");
-            wagePerHour = 20;
-            fullDayHour = 4;
+        switch (presenty) {
+            //switch case
+            case 1:
+                System.out.println("present");
+                wagePerHour = 20;
+                fullDayHour = 8;
+                break;
+            case 2:
+                System.out.println("absent");
+                wagePerHour = 0;
+                fullDayHour = 0;
+                break;
+            default:
+                System.out.println("partTime");
+                wagePerHour = 20;
+                fullDayHour = 4;
         }
-        int dailyWage = wagePerHour*fullDayHour;
-        System.out.println("Daily wage of the employe is:"+dailyWage);
+        int dailyWage = wagePerHour * fullDayHour;
+        System.out.println("Daily wage of the employe is:" + dailyWage);
+
     }
 }
