a8e1bc6adf7f3e915a344fa528485702815fdd31
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 17:36:31 +0530

    till 20 day wage is added

--- main.java
+++ main.java
@@ -6,35 +6,37 @@ public class main {
     public static void main(String[] args) {
 
         int wagePerHour = 20;
-        int fullDayHour = 8;
-        int forMonth = 20;
+        int dailyHour = 8;
+        int totalHour = 0;
+        int wage;
         System.out.println("welcome to employee wage");
         //Random class
         Random random = new Random();
-
-        //object of the random
-        int presenty = random.nextInt(3);
-        switch (presenty) {
-            //switch case
-            case 1:
-                System.out.println("present");
-                wagePerHour = 20;
-                fullDayHour = 8;
-                break;
-            case 2:
-                System.out.println("absent");
-                wagePerHour = 0;
-                fullDayHour = 0;
-                break;
-            default:
-                System.out.println("partTime");
-                wagePerHour = 20;
-                fullDayHour = 4;
+        for (int i = 1; i <= 20; i++) {
+            //object of the random
+            int presenty = random.nextInt(3);
+            switch (presenty) {
+                //switch case
+                case 1:
+                    System.out.println("present");
+                    wagePerHour = 20;
+                    dailyHour = 8;
+                    break;
+                case 2:
+                    System.out.println("absent");
+                    wagePerHour = 0;
+                    dailyHour = 0;
+                    break;
+                default:
+                    System.out.println("partTime");
+                    wagePerHour = 20;
+                    dailyHour = 4;
+            }
+            totalHour = totalHour + dailyHour;
+            System.out.println("total hours works"+ totalHour );
         }
-        int dailyWage = wagePerHour * fullDayHour;
-        forMonth = forMonth*dailyWage;
-        System.out.println("Daily wage of the employe is:" + dailyWage);
-        System.out.println("Monthly wage of the employe is:" + forMonth);
 
+        wage = totalHour * wagePerHour;
+        System.out.println("months wage is" + " " + wage);
     }
 }
