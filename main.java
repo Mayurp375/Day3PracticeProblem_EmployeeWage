1b27536d169865df97a9c104cc5166fa87cedcf7
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 17:01:42 +0530

    employee present or absent

--- main.java
+++ main.java
@@ -1,7 +1,18 @@
 package Day3PracticeProblem_EmployeeWage;
 
+import java.util.Random;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage");
+
+        Random random = new Random();
+
+        int presenty = random.nextInt(2);
+        if(presenty >= 1){
+            System.out.println("present");
+        }else{
+            System.out.println("absent");
+        }
     }
 }
