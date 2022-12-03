a9034c352beefd51dec7ee4ab5676c51e5abad26
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 17:05:43 +0530

    daily wage of the empyee

--- main.java
+++ main.java
@@ -4,15 +4,23 @@ import java.util.Random;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("welcome to employee wage");
 
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        System.out.println("welcome to employee wage");
         Random random = new Random();
 
         int presenty = random.nextInt(2);
-        if(presenty >= 1){
+        if (presenty >= 1) {
             System.out.println("present");
-        }else{
+             wagePerHour = 20;
+             fullDayHour = 8;
+        } else {
             System.out.println("absent");
+            wagePerHour = 0;
+            fullDayHour = 0;
         }
+        int dailyWage = wagePerHour*fullDayHour;
+        System.out.println("Daily wage of the employe is:"+dailyWage);
     }
 }
