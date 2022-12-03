3f5b5e7ad81942677908e56060b230e70189adab
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 17:10:26 +0530

    partTime presenty and wage

--- main.java
+++ main.java
@@ -10,15 +10,20 @@ public class main {
         System.out.println("welcome to employee wage");
         Random random = new Random();
 
-        int presenty = random.nextInt(2);
-        if (presenty >= 1) {
+        int presenty = random.nextInt(3);
+        System.out.println(presenty);
+        if (presenty == 1) {
             System.out.println("present");
              wagePerHour = 20;
              fullDayHour = 8;
-        } else {
+        } else if(presenty ==2) {
             System.out.println("absent");
             wagePerHour = 0;
             fullDayHour = 0;
+        }else {
+            System.out.println("partTime");
+            wagePerHour = 20;
+            fullDayHour = 4;
         }
         int dailyWage = wagePerHour*fullDayHour;
         System.out.println("Daily wage of the employe is:"+dailyWage);
